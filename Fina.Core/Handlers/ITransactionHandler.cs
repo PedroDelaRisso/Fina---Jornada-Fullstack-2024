5b7e2695ab1eea1ce886a56faa0a7c521335a2af
Pedro Delavechia Risso <p.delavechia.r@gmail.com>
2024-05-27 22:26:46 -0300

    Dia 1

---
+++ Fina.Core/Handlers/ITransactionHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Transactions;
+using Fina.Core.Requests.Transactions;
+using Fina.Core.Responses;
+
+namespace Fina.Core.Handlers;
+
+public interface ITransactionHandler
+{
+    
+    Task<Response<Transaction?>> CreateAsync(CreateTransactionRequest request);
+    Task<Response<Transaction?>> UpdateAsync(UpdateTransactionRequest request);
+    Task<Response<Transaction?>> DeleteAsync(DeleteTransactionRequest request);
+    Task<Response<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request);
+    Task<PagedResponse<List<Transaction>?>> GetAllAsync(GetAllTransactionsRequest request);
+}
---
+++ Fina.Core/Requests/Transactions/UpdateTransactionRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Fina.Core.Enums;
+
+namespace Fina.Core.Requests.Transactions;
+
+public class UpdateTransactionRequest
+{
+    public long Id { get; set; }
+    [Required(ErrorMessage = "Título inválido")]
+    public string Title { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Tipo Inválido")]
+    public ETransactionType Type { get; set; } = ETransactionType.Withdraw;
+    [Required(ErrorMessage = "Valor inválido")]
+    public decimal Amount { get; set; }
+    [Required(ErrorMessage = "Categoria inválida")]
+    public long CategoryId { get; set; }
+    [Required(ErrorMessage = "Data inválida")]
+    public DateTime? PaidOrReceveidAt { get; set; }
+}
