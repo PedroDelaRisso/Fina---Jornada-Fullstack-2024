e8e8357d6de2490908251e751d6653f2f8be2e2e
Pedro Delavechia Risso <p.delavechia.r@gmail.com>
2024-05-27 22:33:55 -0300

    Fix referências antigas

--- Fina.Web/Program.cs
+++ Fina.Web/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Fina.App;
+using Fina.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
